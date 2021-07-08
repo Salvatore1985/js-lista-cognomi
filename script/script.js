95d2a5b38bb0a675fcde0a4830e1aebe90b9075f
Salvatore1985 <80257763+Salvatore1985@users.noreply.github.com>
2021-07-08 08:52:32 +0200

    ciclo-for

--- script/script.js
+++ script/script.js
@@ -5,26 +5,49 @@
 4. scrivi anche la posizione "umana" (partendo da 1) della lista in cui il nuovo utente si trova*/
 // enter
 
-//1. chiedi all’utente il cognome
+/* inseriscilo in un array con altri cognomi*/
+var listElement = document.getElementById('list');
+var list_User = ['Bianchi', 'Neri', 'Rossi', 'Verdi', 'Gialli'];
 
-var user = "";
 
+/*creo una var vuota */
+var userLastname;
 
 do {
-    user = prompt('Inserisci il tuo Cognome').trim().toLowerCase();
-} while (console.log(user.toLowerCase()));
+    /* chiedi all’utente il cognome*/
+    userLastname = prompt('Inserisci il tuo Cognome');
+    /*Aggiungiamo il trim() al while 
+    se non c'è userLastname o quando trimmo una stringa vuota non me la fa passare---
+    -attenzione che non lo stiamo trimmando ma lo stiamo solo controllando*/
+} while (!userLastname || userLastname.trim() === '');
 
 
-// var n;
-// var i = 0;
-// var a = Math.pow(2, 1000);
+/*assegno userLastname a se stesso trimmato*/
+userLastname = userLastname.trim();
+console.log(userLastname);
 
-// do {
-//     n = parseInt(prompt('inserisci un numero'));
-// } while (isNaN(n) && n <= 0);
 
-// var i = 1;
-// while (i <= 1000) {
-//     console.log(Math.pow(2 * i));
-//     i++;
-// }
+/*inserisco alla mia Array list_User un elemento con il metodo push()*/
+list_User.push(userLastname);
+console.table(list_User);
+
+
+/*vado a riodinare la mia lista list_User in ordine alfabetica con il metodo sort()*/
+list_User.sort();
+console.table(list_User);
+
+/*stampo la posizione del cognome con il metodo indexOf()aggiungiamo +1 perch'è deve essere in posizione umana */
+var position = list_User.indexOf(userLastname) + 1;
+console.log('Il nuovo Cognome è in posizione ', + position);
+var positionElement = document.getElementById('position');
+positionElement.innerHTML = position;
+
+/* creo una var di dove vado a costruire la nostra lista*/
+var listIteams = '';
+/*creo un ciclo for perché mi serve un contatore*/
+for (i = 0; i < list_User.length; i++) {
+    /*inserisco <li> nel nostro HTML*/
+    listIteams += '<li>' + list_User[i] + '</li>';
+
+}
+listElement.innerHTML = listIteams;
